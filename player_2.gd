bbfe7bcdf9737db17ad2f1bbd2fa67c17dd7e9dc
rks244256 <ryansirisena@gmail.com>
2023-11-14 19:54:54 -0500

    Added Player 2

--- player_2.gd
+++ player_2.gd
@@ -4,8 +4,8 @@ var speed = 10
 
 func _physics_process(delta):
 	var input_dir = Vector2.ZERO
-	var up = Input.is_action_pressed('p1_up')
-	var down = Input.is_action_pressed('p1_down')
+	var up = Input.is_action_pressed('p2_up')
+	var down = Input.is_action_pressed('p2_down')
 	if up:
 		input_dir = Vector2.UP
 	if down:
