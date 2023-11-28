b58bbc2e52145c04389a4ffc9e43761bf9464651
rks244256 <ryansirisena@gmail.com>
2023-11-28 08:35:21 -0500

    Final version. Use for reference.

--- player_1.gd
+++ player_1.gd
@@ -1,14 +1,16 @@
 extends StaticBody2D
 
-var speed = 10
+const speed = 10
 
 func _physics_process(delta):
-	var input_dir = Vector2.ZERO
+	var vector = Vector2.ZERO
 	var up = Input.is_action_pressed('p1_up')
 	var down = Input.is_action_pressed('p1_down')
 	if up:
-		input_dir = Vector2.UP
+		vector = Vector2.UP
 	if down:
-		input_dir = Vector2.DOWN
-	var velocity = input_dir * speed
+		vector = Vector2.DOWN
+		
+	var velocity = vector * speed
+	
 	move_and_collide(velocity)
